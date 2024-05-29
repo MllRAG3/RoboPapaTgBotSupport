19c492a6dbafef0b4f0f5f7bb7e3a5ec24ebb28a
v <t.shagin@gmail.com>
2024-05-29 21:01:28 +0300

    feat: Добавлен механизм сохранение идей
    Исправлены ошибки:
    * -
    
    Добавлены:
    * Возможность добавлять идеи
    * Защита от спама
    
    Удалены:
    * -

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from modules.domain.executors import Exec
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    pass
+    Exec(message).start_greet()
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'add_idea')
 def add_idea(call):
-    pass
+    Exec(call.message, user=call.from_user).add_idea()
 
 
 if __name__ == '__main__':
--- modules/domain/executors.py
+++ modules/domain/executors.py
@@ -1,6 +1,8 @@
-from telebot.types import User, Message
+from telebot.types import User, Message, InlineKeyboardMarkup, InlineKeyboardButton
+from datetime import datetime
 
 from modules.constants.telegram_bot import bot
+import modules.constants.bot_messages as bm
 from modules.database.models.tg_users import TgUsers
 from modules.database.models.user_ideas import UserIdeas
 
@@ -9,6 +11,24 @@ class Exec:
     def __init__(self, message: Message, user: User | None = None):
         self.message: Message = message
         self.tb_user: User = message.from_user if user is None else user
+        self.db_user: TgUsers = TgUsers.get_or_create(telegram_id=self.tb_user.id, chat_id=message.chat.id)[0]
+        self.db_user.last_update = datetime.now()
+        TgUsers.save(self.db_user)
 
     def start_greet(self):
-        pass
+        buttons = InlineKeyboardMarkup().add(InlineKeyboardButton('Оставить идею', callback_data='add_idea'))
+        bot.send_message(self.message.chat.id, bm.GREET, reply_markup=buttons)
+
+    def add_idea(self):
+        bot.send_message(self.message.chat.id, bm.INPUT_IDEA)
+        bot.register_next_step_handler(self.message, callback=self.register_idea)
+
+    def register_idea(self, message: Message):
+        if len(message.text) > 1000:
+            bot.send_message(self.message.chat.id, bm.SOMETHING_WENT_WRONG)
+            return
+
+        data = {"user": self.db_user, "idea": message.text.strip()}
+        UserIdeas.create(**data)
+        buttons = InlineKeyboardMarkup().add(InlineKeyboardButton('Предложить еще идей', callback_data='add_idea'))
+        bot.send_message(self.message.chat.id, bm.THANKS_FOR_SUPPORTING, reply_markup=buttons)
